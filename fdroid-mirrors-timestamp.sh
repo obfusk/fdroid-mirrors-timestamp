dca56e5abcbf8b66a65453ea2acb26e5a462dcf6
FC Stegerman <flx@obfusk.net>
2023-10-03 01:53:07 +0200

    ...

--- fdroid-mirrors-timestamp.sh
+++ fdroid-mirrors-timestamp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
 mirrors=(
   https://f-droid.org
   https://fdroid.tetaneutral.net/fdroid
@@ -11,12 +12,49 @@ mirrors=(
   https://mirror.ossplanet.net/fdroid
   https://plug-mirror.rcac.purdue.edu/fdroid    # primary
 )
-for mirror in "${mirrors[@]}"; do
+
+onion_mirrors=(
+  http://fdroidorg6cooksyluodepej4erfctzk7rrjpjbbr6wx24jh3lqyfwyd.onion/fdroid
+  http://ftpfaudev4triw2vxiwzf4334e3mynz7osqgtozhbc77fixncqzbyoyd.onion/fdroid
+  http://lysator7eknrfl47rlyxvgeamrv7ucefgrrlhk7rouv3sna25asetwid.onion/pub/fdroid
+)
+
+check_mirror() {
+  local mirror="$1" onion="$2"
+  if [ "$onion" = --onion ]; then
+    curl=( curl --socks5-hostname localhost:9050 -s )
+  else
+    curl=( curl -s )
+  fi
   for component in repo archive; do
-    if ts="$(curl -s "$mirror"/"$component"/entry.json | jq -r .timestamp 2>/dev/null)"; then
+    if ts="$( "${curl[@]}" "$mirror"/"$component"/entry.json \
+              | jq -r .timestamp 2>/dev/null )"; then
       echo "$mirror [$component]:"
-      date +'%FT%T %Z' -d @"$(( "$ts" / 1000 ))"
+      ts="$(date +'%FT%T %Z' -d @"$(( "$ts" / 1000 ))")"
+      if [ "$mirror" = "${mirrors[0]}" ]; then
+        if [ "$component" = repo ]; then
+          fdroid_repo_ts="$ts"
+        else
+          fdroid_archive_ts="$ts"
+        fi
+      elif [ -t 1 ] && [[ ( "$component" == repo && \
+                            "$ts" != "$fdroid_repo_ts" ) || \
+                          ( "$component" == archive && \
+                            "$ts" != "$fdroid_archive_ts" ) ]]; then
+        ts="\033[0;31m$ts\033[0m"
+      fi
+      echo -e "$ts"
     fi
   done
   echo
+}
+
+for mirror in "${mirrors[@]}"; do
+  check_mirror "$mirror"
 done
+
+if [ "$2" = --onions ]; then
+  for mirror in "${onion_mirrors[@]}"; do
+    check_mirror "$mirror" --onion
+  done
+fi
