a707f27e7c5b25c80df2d980062617199d8fede4
FC (Fay) Stegerman <flx@obfusk.net>
2023-10-05 21:27:58 +0200

    use fdroid-statuspage-deployment submodule

--- fdroid-mirrors-timestamp.sh
+++ fdroid-mirrors-timestamp.sh
@@ -1,75 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+statuspage_yml=fdroid-statuspage-deployment/group_vars/all.yml
+mirrors=() onion_mirrors=() unofficial_mirrors=() unofficial_onion_mirrors=()
 check_onion=no check_unofficial=no exitcode=0
-
 usage='Usage: fdroid-mirrors-timestamp.sh [--onion] [--unofficial]'
-
-if [ -e fdroid-statuspage-deployment ]; then
-  yml=fdroid-statuspage-deployment/group_vars/all.yml
-  mirrors=() onion_mirrors=()
-  while read -r; do
-    if [[ "$REPLY" == *.onion* ]]; then
-      onion_mirrors+=( "$REPLY" )
-    else
-      mirrors+=( "$REPLY" )
-    fi
-  done < <( yq -r < "$yml" '.mirrors[] | select(.official) | .base_url' | sort )
-  unofficial_mirrors=() unofficial_onion_mirrors=()
-  while read -r; do
-    if [[ "$REPLY" == *.onion* ]]; then
-      unofficial_onion_mirrors+=( "$REPLY" )
-    else
-      unofficial_mirrors+=( "$REPLY" )
-    fi
-  done < <( yq -r < "$yml" '.mirrors[] | select(.official | not) | .base_url' | sort )
-else
-  mirrors=(
-    https://f-droid.org
-    https://fdroid.tetaneutral.net/fdroid
-    https://ftp.agdsn.de/fdroid                   # primary
-    https://ftp.fau.de/fdroid
-    https://ftp.lysator.liu.se/pub/fdroid         # primary
-    https://mirror.cyberbits.eu/fdroid
-    https://mirror.fcix.net/fdroid
-    https://mirror.ossplanet.net/fdroid
-    https://plug-mirror.rcac.purdue.edu/fdroid    # primary
-  )
-
-  onion_mirrors=(
-    http://fdroidorg6cooksyluodepej4erfctzk7rrjpjbbr6wx24jh3lqyfwyd.onion/fdroid
-    http://ftpfaudev4triw2vxiwzf4334e3mynz7osqgtozhbc77fixncqzbyoyd.onion/fdroid
-    http://lysator7eknrfl47rlyxvgeamrv7ucefgrrlhk7rouv3sna25asetwid.onion/pub/fdroid
-  )
-
-  unofficial_mirrors=(
-    https://bubu1.eu/fdroid
-    https://cloudflare.f-droid.org
-    https://fdroid.astra.in.ua/fdroid
-    https://forksystems.mm.fcix.net/fdroid
-    https://ftp.gwdg.de/pub/android/fdroid
-    https://ftp.osuosl.org/pub/fdroid
-    https://ftp.snt.utwente.nl/pub/software/fdroid
-    https://mirror.albony.xyz/fdroid
-    https://mirror.cxserv.de/fdroid
-    https://mirror.freedif.org/fdroid
-    https://mirror.kumi.systems/fdroid
-    https://mirror.librelabucm.org/fdroid
-    https://mirror01.komogoto.com/fdroid
-    https://mirrors.dotsrc.org/fdroid
-    https://mirrors.jevincanders.net/fdroid
-    https://mirrors.nju.edu.cn/fdroid
-    https://mirrors.tuna.tsinghua.edu.cn/fdroid
-    https://opencolo.mm.fcix.net/fdroid
-    https://southfront.mm.fcix.net/fdroid
-    https://uvermont.mm.fcix.net/fdroid
-    https://ziply.mm.fcix.net/fdroid
-  )
-
-  unofficial_onion_mirrors=(
-    http://mirror.ossplanetnyou5xifr6liw5vhzwc2g2fmmlohza25wwgnnaw65ytfsad.onion/fdroid
-  )
-fi
 
 check_mirror() {
   local mirror="$1" onion="${2:-}"
@@ -114,6 +49,22 @@ check_mirror() {
   done
   echo
 }
+
+while read -r url; do
+  if [[ "$url" == *.onion* ]]; then
+    onion_mirrors+=( "$url" )
+  else
+    mirrors+=( "$url" )
+  fi
+done < <( yq -r < "$statuspage_yml" '.mirrors[] | select(.official) | .base_url' | sort )
+
+while read -r url; do
+  if [[ "$url" == *.onion* ]]; then
+    unofficial_onion_mirrors+=( "$url" )
+  else
+    unofficial_mirrors+=( "$url" )
+  fi
+done < <( yq -r < "$statuspage_yml" '.mirrors[] | select(.official | not) | .base_url' | sort )
 
 for arg in "$@"; do
   case "$arg" in
