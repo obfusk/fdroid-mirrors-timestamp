ffe5cb28b44a51186695fbd8a2b37fe2567ca05e
FC (Fay) Stegerman <flx@obfusk.net>
2023-10-05 20:07:16 +0200

    ...

--- fdroid-mirrors-timestamp.sh
+++ fdroid-mirrors-timestamp.sh
@@ -19,6 +19,34 @@ onion_mirrors=(
   http://lysator7eknrfl47rlyxvgeamrv7ucefgrrlhk7rouv3sna25asetwid.onion/pub/fdroid
 )
 
+unofficial_mirrors=(
+  https://bubu1.eu/fdroid
+  https://cloudflare.f-droid.org
+  https://fdroid.astra.in.ua/fdroid
+  https://forksystems.mm.fcix.net/fdroid
+  https://ftp.gwdg.de/pub/android/fdroid
+# https://ftp.osuosl.org/pub/fdroid
+  https://ftp.snt.utwente.nl/pub/software/fdroid
+  https://mirror.albony.xyz/fdroid
+  https://mirror.cxserv.de/fdroid
+  https://mirror.freedif.org/fdroid
+  https://mirror.kumi.systems/fdroid
+  https://mirror.librelabucm.org/fdroid
+# https://mirror01.komogoto.com/fdroid
+  https://mirrors.dotsrc.org/fdroid
+  https://mirrors.jevincanders.net/fdroid
+  https://mirrors.nju.edu.cn/fdroid
+  https://mirrors.tuna.tsinghua.edu.cn/fdroid
+  https://opencolo.mm.fcix.net/fdroid
+  https://southfront.mm.fcix.net/fdroid
+  https://uvermont.mm.fcix.net/fdroid
+  https://ziply.mm.fcix.net/fdroid
+)
+
+unofficial_onion_mirrors=(
+  http://mirror.ossplanetnyou5xifr6liw5vhzwc2g2fmmlohza25wwgnnaw65ytfsad.onion/fdroid
+)
+
 check_mirror() {
   local mirror="$1" onion="${2:-}"
   if [ "$onion" = --onion ]; then
@@ -49,12 +77,41 @@ check_mirror() {
   echo
 }
 
+check_onion=no check_unofficial=no
+
+for arg in "$@"; do
+  case "$arg" in
+    --onion)
+      check_onion=yes
+    ;;
+    --unofficial)
+      check_unofficial=yes
+    ;;
+    *)
+      echo "Unknown option: $arg" >&2
+      echo "Usage: fdroid-mirrors-timestamp.sh [--onion] [--unofficial]" >&2
+      exit 1
+    ;;
+  esac
+done
+
 for mirror in "${mirrors[@]}"; do
   check_mirror "$mirror"
 done
 
-if [ "${1:-}" = --onions ]; then
+if [ "$check_unofficial" = yes ]; then
+  for mirror in "${unofficial_mirrors[@]}"; do
+    check_mirror "$mirror"
+  done
+fi
+
+if [ "$check_onion" = yes ]; then
   for mirror in "${onion_mirrors[@]}"; do
     check_mirror "$mirror" --onion
   done
+  if [ "$check_unofficial" = yes ]; then
+    for mirror in "${unofficial_onion_mirrors[@]}"; do
+      check_mirror "$mirror" --onion
+    done
+  fi
 fi
